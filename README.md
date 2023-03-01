63f7dc9564b431e44c2ca48db0eeb9ea6a976cc4
lsilencej <43839502+lsilencej@users.noreply.github.com>
2023-03-01 23:37:52 +0800

    upload project

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# picgo-plugin-lsky
-picgo插件，用于lskypro兰空图床上传
+## picgo-plugin-lsky-uploader
+
+为基于 [PicGo-Core](https://github.com/PicGo/PicGo-Core) 开发的项目提供的 uploader，用于上传至服务器自建的 [lsky-pro](https://github.com/lsky-org/lsky-pro)，即开源版的兰空图床，兼容最新版api，底层请求已按照文档适配新版 picgo，目前已在最新版 [PicList](https://github.com/Kuingsmile/PicList) （第三方基于 [PicGo-Core](https://github.com/PicGo/PicGo-Core) 开发的项目）上进行测试，测试结果返回成功
+
+## 安装
+
+- npm
+
+  npm install picgo-plugin-lsky-uploader
+
+- PicList
+
+  - 在线安装
+
+    插件设置当中搜索 lsky-uploader
+
+  - 离线安装
+
+    克隆本仓库，复制项目到 PicList 配置目录，执行 `npm install ./picgo-plugin-lsky-uploader`，然后重启应用即可
+
+## 参数
+
+|  参数名称   |                             描述                             |            示例             | 是否必须 |
+| :---------: | :----------------------------------------------------------: | :-------------------------: | :------: |
+| 服务器域名  |              服务器域名，不需要填补具体上传接口              | https://image.lsilencej.top |    是    |
+| 获取的Token | 通过自行调用获取 token 的 api 获取的 token，不需要添加 Bearer |      1\|11111111111111      |    是    |
+
+## 截图
+
+![image-20230301225114449](https://image.lsilencej.top/i/2023/03/01/63ff6663dfac9.png)
+
---
+++ src/index.js
@@ -0,0 +1,91 @@
+module.exports = (ctx) => {
+	const register = () => {
+		ctx.helper.uploader.register('lsky-uploader', {
+			handle,
+			config: config,
+			name: 'lsky'
+		})
+	}
+	return {
+		uploader: 'lsky-uploader',
+		register
+	}
+}
+
+const handle = async (ctx) => {
+	let userConfig = ctx.getConfig('picBed.lsky-uploader')
+	if (!userConfig) {
+		throw new Error('Can\'t find uploader config')
+	}
+	const Url = userConfig.Url
+	const Token = userConfig.Token
+
+	const imgList = ctx.output
+	for (let i in imgList) {
+		let image = imgList[i].buffer
+		if (!image && imgList[i].base64Image) {
+			image = Buffer.from(imgList[i].base64Image, 'base64')
+		}
+		const postConfig = postOptions(Url, Token, imgList[i].fileName, image)
+		let body = await ctx.request(postConfig)
+		body = JSON.parse(body)
+		if (body.status) {
+			delete imgList[i].base64Image
+			delete imgList[i].buffer
+			imgList[i].imgUrl = body.data.links.url
+		} else {
+			ctx.emit('notification', {
+				title: '上传失败',
+				body: body.message
+			})
+			throw new Error(body.message)
+		}
+	}
+	return ctx
+}
+
+const postOptions = (Url, Token, fileName, image) => {
+	return {
+		method: 'POST',
+		url: Url + `/api/v1/upload`,
+		headers: {
+			contentType: 'multipart/form-data',
+			'Accept': 'application/json',
+			'Authorization': `Bearer ` + Token,
+			'User-Agent': 'PicGo'
+		},
+		formData: {
+			file: {
+				value: image,
+				options: {
+					filename: fileName
+				}
+			},
+			ssl: 'true'
+		}
+	}
+}
+
+const config = ctx => {
+	let userConfig = ctx.getConfig('picBed.lsky-uploader')
+	if (!userConfig) {
+		userConfig = {}
+	}
+	return [{
+			name: 'Url',
+			type: 'input',
+			default: userConfig.Url,
+			required: true,
+			message: '服务器域名',
+			alias: '服务器域名'
+		},
+		{
+			name: 'Token',
+			type: 'input',
+			default: userConfig.Token,
+			required: true,
+			message: '获取的Token',
+			alias: '获取的Token'
+		}
+	]
+}
